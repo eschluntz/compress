f5d2b928578ee5f838e7ba69c730072aa6fa042a
Erik Schluntz <eschluntz@gmail.com>
2023-08-23 15:22:06 -0700

    script for CI

---
+++ install.sh
@@ -0,0 +1,6 @@
+# unified install script for local and CI
+
+sudo apt-get -y install libenchant-2-dev
+
+pip install --upgrade pip
+pip install -r requirements.txt
---
+++ run_test.sh
@@ -0,0 +1,10 @@
+## Unified script to run identical checks locally and in CI
+
+# exit when any command fails
+set -e
+
+# linter
+# pylint $(git ls-files '*.py')
+
+# tests
+pytest
