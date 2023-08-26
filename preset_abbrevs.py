d236936f670df9db67df1db2cae82d2af587a982
Erik Schluntz <eschluntz@gmail.com>
2023-08-26 15:43:35 -0700

    tweaks

--- preset_abbrevs.py
+++ preset_abbrevs.py
@@ -2,46 +2,63 @@
 
 """
 Abbrevs you definitely want in your set.
-you can set None if you want to pick one automatically
-you're responsible for creating any plurals here
+Use this list to protect abbreviations that the algorithm 
+may assign to other words, or more common abbreviations that deviate
+from the algorithm, ie bc -> because.
+
+Use the blacklist to block assignments that you type frequently but aren't words.
 """
 
 
 PRESET_ABBREVS = {
-#     "right now": "rn",
-#     "again": "ag",
-#     "actually": "ac",
-#     "anything": "ay",
-#     "every": "e",
-#     "everyone": "en",
-#     "everything": "et",
-#     "some": "s",
-#     "someone": "sn",
-#     "something": "st",
-#     "hardware": "hw",
-#     "software": "sw",
-#     "without": "wo",
-#     "maybe": "mb",
-#     "question": "q",
-#     "questions": "qs",
-#     "meeting": "mtg",
-#     "meetings": "mtgs",
-#     "very": "v",
-#     "fix": "x",
-#     "if i remember correctly": "iirc",
-#     "let me know": "lmk",
-#     "definitely": "def",
-#     "I'll keep you posted": "ikyp",
-#     """Best,
-# - Erik""": "bst",
-#     "calendar invite": "cali",
-#     "screenshot": "sc",
-#     "Can you post a screenshot?": "csc",
+    "and I": "ni",
+    "again": "ag",
+    "actually": "ac",
+    "anything": "ay",
+
+    "every": "e",
+    "everyone": "en",
+    "everything": "et",
+
+    "some": "s",
+    "someone": "sn",
+    "something": "st",
+
+    "hardware": "hw",
+    "software": "sw",
+
+    "with": "w",
+    "without": "wo",
+
+    "week": "wk",
+    "what": "wt",
+    "whatever": "wte",
+    "when": "wn",
+    "whenever": "wne",
+    "where": "wr",
+    "wherever": "wre",
+    "which": "wh",
+    "whichever": "whe",
+
+    "maybe": "mb",
+    "question": "q",
+    "questions": "qs",
+    "meeting": "mtg",
+    "meetings": "mtgs",
+    "very": "v",
+    "fix": "x",
+    "if I remember correctly": "iirc",
+    "let me know": "lmk",
+    "definitely": "def",
+    "I'll keep you posted": "ikyp",
+    "calendar invite": "cali",
+    "screenshot": "sc",
+    "Can you post a screenshot?": "csc",
 }
 
 # these will not be used as abbrevs
 BLACKLIST = set([
-    "a", "i", "it", "an", "int", "so",
+    "a", "ai", "alt", "i", "id", "it", "an", "int", "so",
     "is", "re", "we", "the", "in", "as",
     "no", "ie", "eg", "me", "be", "at", "do",
     "talk", "to",
